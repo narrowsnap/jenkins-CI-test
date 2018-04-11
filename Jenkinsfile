fa91baed7ad0eb009363b8d9d841eb4cf5aeeaae
yangz <yangz@keyayun.com>
2018-04-11 15:40:41 +0800

    add test

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,24 +1,13 @@
 pipeline {
   agent {
     docker {
-      image 'node:carbon'
+        image 'node:carbon'
     }
-
   }
   stages {
     stage('Initialize') {
       steps {
         echo 'jenkins test'
-      }
-    }
-    stage('Build') {
-      steps {
-        sh 'npm install'
-      }
-    }
-    stage('Test') {
-      steps {
-        sh 'npm test'
       }
     }
   }
--- test/index.js
+++ test/index.js
@@ -7,7 +7,18 @@ const request = require('supertest').agent(app.listen());
 
 const expect = chai.expect;
 
-describe('first test', () => {
+describe('加法函数的测试', function() {
+    it('1 加 1 应该等于 2', function() {
+        expect(1 + 1).to.be.equal(2);
+    });
+});
+
+
+/*describe('first test', () => {
+    it('result should be 2', (done) => {
+        expect(1+1).to.be.equal(2)
+        done()
+    })
     it('get /', () => {
         request.get('/')
             .expect(200)
@@ -15,4 +26,4 @@ describe('first test', () => {
                 expect(res).to.be.an('object')
             })
     })
-})
+})*/
